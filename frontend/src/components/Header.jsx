ec32264c1331da1578d1db8a9df39fba818d2f51
thy-2004 <thy230200@gmail.com>
2025-10-15 10:55:20 +0700

    okk 6

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
         <Container>
           <Navbar.Brand as={Link} to='/'>
             <img src={logo} alt='ProShop' />
-            ProShop
+            ProShopppp
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
